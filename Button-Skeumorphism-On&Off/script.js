1baf9c567ead5077567db7219e51de32156b0c6d
DANIEL TOBON <127164388+Danitobon7@users.noreply.github.com>
2023-08-28 15:54:51 -0500

    Add files via upload

--- Button-Skeumorphism-On&Off/script.js
+++ Button-Skeumorphism-On&Off/script.js
@@ -1,10 +1,28 @@
-// Force a hover to see the effect
-const share = document.querySelector('.share');
+let onSwitch = document.getElementById("on");
+let offSwitch = document.getElementById("off");
+let onLabel = document.querySelector("label[for='on']");
+let offLabel = document.querySelector("label[for='off']");
+let container = document.getElementById("container");
 
-setTimeout(() => {
-  share.classList.add("hover");
-}, 1000);
+let onClicked = () => {
+  container.style.boxShadow = "inset -0.9em 0 0.9em #494949";
+  offLabel.style.color = "#8e8e8e";
+  onLabel.style.color = "#98f195";
+};
 
-setTimeout(() => {
-  share.classList.remove("hover");
-}, 3000);
+let offClicked = () => {
+  container.style.boxShadow = "inset 0.9em 0 0.9em #171717";
+  onLabel.style.color = "#8e8e8e";
+  offLabel.style.color = "#f1959f";
+};
+
+window.addEventListener("load", () => {
+  if (onSwitch.checked) {
+    onClicked();
+  } else {
+    offClicked();
+  }
+});
+
+onSwitch.addEventListener("click", onClicked);
+offSwitch.addEventListener("click", offClicked);
